4b171ac88fd5e82afa8d5c508529d857e5c83407
Maynard Peralta <maynperalta@gmail.com>
2020-08-30 19:47:24 -0600

    updated README

--- app.js
+++ app.js
@@ -17,18 +17,18 @@ const teamMembers = [];
 function createManager() {
     inquirer.prompt([
         {
-            message: "What is the manager's name?",
+            message: "Please enter the manager's name.",
             type: "input",
             name: "name",
             validate: async (input) => {
-                if (input == "" || /\s/.test(input)) {
+                if (input == "") {
                     return "Please enter a valid first or last name!";
                 }
                 return true;
             }
         },
         {
-            message: "What is the manager's ID?",
+            message: "Please enter the manager's ID.",
             type: "input",
             name: "id",
             validate: async (input) => {
@@ -40,7 +40,7 @@ function createManager() {
 
         },
         {
-            message: "What is the manager's email address?",
+            message: "Please enter the manager's email address.",
             type: "input",
             name: "email",
             validate: async (input) => {
@@ -51,7 +51,7 @@ function createManager() {
             }
         },
         {
-            message: "What is the manager's office phone number?",
+            message: "Please enter the manager's office phone number.",
             type: "input",
             name: "officeNumber",
             validate: async (input) => {
@@ -108,7 +108,7 @@ function createEngineer() {
             }
         },
         {
-            message: "What is the engineer's ID?",
+            message: "Please enter the engineer's ID.",
             type: "input",
             name: "id",
             validate: async (input) => {
@@ -163,7 +163,7 @@ function createIntern() {
             }
         },
         {
-            message: "What is the intern's ID?",
+            message: "Please enter the intern's ID.",
             type: "input",
             name: "id",
             validate: async (input) => {
@@ -185,7 +185,7 @@ function createIntern() {
             }
         },
         {
-            message: "What school did the intern attend?",
+            message: "Please enter the school the intern is enrolled in.",
             type: "input",
             name: "school",
             validate: async (input) => {
@@ -203,8 +203,6 @@ function createIntern() {
     })
 }
 
-console.log(teamMembers);
-
 createManager()
 
 const saveUsers = () => {
@@ -213,28 +211,3 @@ const saveUsers = () => {
     }
     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
   };
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
